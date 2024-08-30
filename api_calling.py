9dbfe3dc813dadb63560be3a5acee094878248fa
arpitparekh <arpitparekh9@gmail.com>
2024-08-30 09:39:55 +0530

    api calling

--- api_calling.py
+++ api_calling.py
@@ -4,12 +4,17 @@
 from requests import get
 
 productList = []
+randomUser = {}
 
 url = "https://fakestoreapi.com/products"
+url2 = "https://randomuser.me/api/"
 response = get(url)
+response2 = get(url2)
 
 print(response.status_code)
 productList = response.json()
+randomUser = response2.json()
+print(randomUser)
 
 for i in productList:
   print('Title =',i["title"])
@@ -18,4 +23,11 @@ for i in productList:
   print("Image = ",i["image"])
   print()
 
-
+for i in randomUser['results']:
+  print(i['name']['first'],i['name']['last'])
+  print(i['email'])
+  print(i['gender'])
+  print(i['location']['street']['number'],i['location']['street']['name'])
+  print(i['location']['city'],i['location']['state'],i['location']['country'])
+  print(i['location']['postcode'])
+  print()
