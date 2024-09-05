1f120737c87f03f351789de865ff45a4891ff0e1
arpitparekh <arpitparekh9@gmail.com>
2024-09-05 09:10:19 +0530

    update

---
+++ list_widget.py
@@ -0,0 +1,32 @@
+# Tk()
+# list of widget in tkinter
+# listbox
+# grid pack place
+# label
+# button
+# menubutton
+# entry
+# scrollbar
+# radiobutton
+# checkbutton
+# messagebox
+# combobox
+# canvas
+# frame
+# Treeview
+
+# give me more widget list
+# Spinbox
+# scale
+# date time picker
+# colorchooser
+# filedialog
+# messagebox
+# fontchooser
+# progressbar
+
+# remaining
+# menu
+# image
+
+
